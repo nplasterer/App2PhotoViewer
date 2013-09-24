d3084c92eaf6c24850da447aa0eb974b80a74973
Naomi Plasterer <nplaster@mymail.mines.edu>
2013-09-24 10:05:36 -0600

    Updated header with required information

--- BossoPlastererDiviness_PhotoViewer/src/com/mines/bossoplastererdiviness_photoviewer/MainActivity.java
+++ BossoPlastererDiviness_PhotoViewer/src/com/mines/bossoplastererdiviness_photoviewer/MainActivity.java
@@ -1,9 +1,21 @@
 package com.mines.bossoplastererdiviness_photoviewer;
 
+/*Emulator Options
+ * device: Nexus 7
+ * target: JellyBean 4.1.2 SDK version 16
+ */
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+
+/**
+ * Description:
+ * 
+ * Documentation Statement:
+ * 
+ * @author Naomi Plasterer Brandon Bosso Austin Diviness
+ */
 public class MainActivity extends Activity {
 
 	@Override
