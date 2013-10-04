44c092fd6eb98c9ac98fd800b6931666209d2005
Brandon M. Bosso <brbosso@bb136-10.mines.edu>
2013-10-04 17:57:58 -0600

    Added timer and logic to control slideshow

--- BossoPlastererDiviness_PhotoViewer/src/com/mines/bossoplastererdiviness_photoviewer/SlideshowActivity.java
+++ BossoPlastererDiviness_PhotoViewer/src/com/mines/bossoplastererdiviness_photoviewer/SlideshowActivity.java
@@ -1,9 +1,14 @@
 package com.mines.bossoplastererdiviness_photoviewer;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
 
 import com.dropbox.sync.android.DbxFileSystem;
 
@@ -17,6 +22,11 @@ import com.dropbox.sync.android.DbxFileSystem;
 public class SlideshowActivity extends Activity {
 	private DbxFileSystem dbxFs = null;
 	private DownloadImagesTask downloadTask;
+	private ArrayList<Bitmap> images;
+	private int imageIndex;
+	private Timer timer;
+	private int delay;
+	private int period;
 	
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -26,6 +36,11 @@ public class SlideshowActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_slideshow);
 		dbxFs = (DbxFileSystem) getIntent().getExtras().getSerializable("dbxFs");
+		images = new ArrayList<Bitmap>();
+		imageIndex = 0;
+		delay = 0;
+		period = 3000;
+		timer = new Timer();
 	}
 	
 	@Override
@@ -33,6 +48,22 @@ public class SlideshowActivity extends Activity {
 		super.onStart();
 		downloadTask = new DownloadImagesTask(this);
 		downloadTask.execute();
+	}
+	
+	public void startSlideshow() {
+		imageIndex = 0;
+		slideshow();
+	}
+	
+	public void slideshow() {
+		timer.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				int index = imageIndex % images.size();
+				ImageView container = (ImageView) findViewById(R.id.slideshow_container);
+				container.setImageBitmap(images.get(index));
+				imageIndex++;
+			}
+		}, delay, period);
 	}
 
 	/* (non-Javadoc)
