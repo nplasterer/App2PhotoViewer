ebc871bba4717272fb398f50d2f51d75b8c777b5
Brandon M. Bosso <brbosso@bb136-10.mines.edu>
2013-10-05 15:41:54 -0600

    Fixed DownloadImagesTask

---
+++ BossoPlastererDiviness_PhotoViewer/src/com/mines/bossoplastererdiviness_photoviewer/DownloadImagesTask.java
@@ -0,0 +1,95 @@
+package com.mines.bossoplastererdiviness_photoviewer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileInfo;
+import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
+
+public class DownloadImagesTask extends AsyncTask<DbxFileSystem, Void, Boolean> {
+
+	private ProgressDialog loadingDialog;
+	private Activity activity;
+	private OnTaskCompleted listener;
+	private ArrayList<Bitmap> images;
+	private List<DbxFileInfo> filesInfo;
+	private File storageDir;
+	public static final String PATH = "/";
+	
+	public DownloadImagesTask(Activity activity, OnTaskCompleted listener) {
+		this.activity = activity;
+		this.listener=listener;
+		filesInfo = new ArrayList<DbxFileInfo>();
+		images = new ArrayList<Bitmap>();
+	}
+	@Override
+	protected void onPreExecute() {
+		loadingDialog = new ProgressDialog(activity);
+		loadingDialog.setTitle("Downloading Images");
+		loadingDialog.setMessage("Please wait...");
+		loadingDialog.show();
+	}
+
+	@Override
+	protected Boolean doInBackground(DbxFileSystem... params) {
+		BitmapFactory bitmapFactory = new BitmapFactory();
+		try{
+			DbxFileSystem filesystem = params[0];
+			DbxPath path = new DbxPath(PATH);
+			filesInfo = filesystem.listFolder(path);
+			Log.d("mine", filesInfo.size());
+			for (DbxFileInfo fileInfo: filesInfo) {
+				DbxFile file = filesystem.open(fileInfo.path);
+				Bitmap image = bitmapFactory.decodeStream(file.getReadStream());
+				saveFile(image, fileInfo.path.getName().split(".")[0]);
+				file.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			loadingDialog.dismiss();
+		}
+		return true;
+	}
+
+	
+
+
+	@Override 
+	protected void onPostExecute(Boolean result) {
+		listener.onTaskCompleted();	
+	}
+	
+	protected void saveFile(Bitmap image, String filename) {
+		filename += ".png";
+		Log.d("mine", "savingfile"+filename);
+		try {
+			FileOutputStream fileOutStream = activity.openFileOutput(filename, Context.MODE_PRIVATE);
+			image.compress(Bitmap.CompressFormat.PNG, 90, fileOutStream);
+			fileOutStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	
+
+}
